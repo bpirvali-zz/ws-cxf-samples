ace9e3f3757dccb98e7b88469a0a65dd1010ea1d
Behzad Pirvali <bpirvali@gmail.com>
2012-10-08 15:32:35 -0700

    Fixing the cxf-generated BookResource.java

--- BookServiceWebApp/src/main/java/com/bp/bs/BookResource.java
+++ BookServiceWebApp/src/main/java/com/bp/bs/BookResource.java
@@ -3,22 +3,37 @@
 **/
 package com.bp.bs;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.Context;
-import javax.ws.rs.core.Request;
 import javax.ws.rs.core.Response;
 
-import org.apache.cxf.jaxrs.model.wadl.ElementClass;
-
-@Path("/books/{isbn}")
+@Path("/books")
 public interface BookResource {
+
+    @POST
+    @Consumes("application/xml")
+    @Produces("application/octet-stream")
+    Response add(BookState bookstate);
+
+    @DELETE
+    @Produces("application/octet-stream")
+    @Path("/{isbn}")
+    Response deleteIsbn(@PathParam("isbn") String isbn);
 
     @GET
     @Produces("application/xml")
-    @ElementClass(response = BookState.class)
-    Response get(@Context Request request, @PathParam("isbn") String isbn);
+    @Path("/{isbn}")
+    BookState getIsbn(@PathParam("isbn") String isbn);
 
+    @PUT
+    @Consumes("application/xml")
+    @Produces("application/octet-stream")
+    @Path("/{isbn}")
+    Response update(@PathParam("isbn") String isbn, BookState bookstate);
 }
